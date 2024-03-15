f4ad38e1ddf7836a9bd26d4ffa7f57a7d2254bfa
Luis Mendes <machadoluismendes@gmail.com>
2024-03-15 12:39:22 -0300

    Increment
    div Vercel deploy  ChatGPT

--- components/ProfileClient.tsx
+++ components/ProfileClient.tsx
@@ -10,12 +10,15 @@ export default function ProfileClient() {
   if (error) return <div>{error.message}</div>;
 
   return (
-    user && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
+<div>
+  {user && (
+    <div>
+      {/* Use optional chaining to safely access user.picture */}
+      <img src={user.picture ?? '/placeholder-image.jpg'} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  )}
+</div>
   );
 }
