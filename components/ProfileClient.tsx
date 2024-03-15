cf2450697777d60ea18faf50b7fca4eb4873aa95
Luis Mendes <machadoluismendes@gmail.com>
2024-03-15 09:39:39 -0300

    Increment
    TypeScript import

--- components/ProfileClient.tsx
+++ components/ProfileClient.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useUser } from '@auth0/nextjs-auth0/client';
+import React from 'react';
 
 export default function ProfileClient() {
   const { user, error, isLoading } = useUser();
